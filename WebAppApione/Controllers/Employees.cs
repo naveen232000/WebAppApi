5c701b14618c8b217f554a47d60865be6e7fbd95
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-07 14:31:15 +0530

    Add project files.

---
+++ WebAppApione/Controllers/Employees.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppApione.Models;
+
+namespace WebAppApione.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Employees : ControllerBase
+    {
+        static List<Employee>list=new List<Employee>() { 
+        new Employee(){Id=1, FName="Sam",Lname="Kumar",Salary=56326},
+        new Employee(){Id=2, FName="Jon",Lname="Son",Salary=56326},
+        new Employee(){Id=3, FName="Leo",Lname="Das",Salary=56326},
+        new Employee(){Id=4, FName="Geo",Lname="Fenc",Salary=56326},
+        new Employee(){Id=5, FName="Navi",Lname="Ken",Salary=56326},
+        };
+        //GET
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return list;
+        }
+        //GET BY ID
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+           Employee emp =list.SingleOrDefault(x => x.Id == id);
+            if(emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(emp);
+            }
+        }
+        //DELETE
+        [HttpDelete("{id}")]
+        public ActionResult<Employee> Delete(int id) 
+        {
+            Employee emp = list.SingleOrDefault(x => x.Id == id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                list.Remove(emp);
+                return NoContent();
+            }
+        }
+        //POST CREATE
+        [HttpPost]
+        public ActionResult<Employee> Post(Employee emp)
+        {
+          list.Add(emp);
+            return (CreatedAtAction(nameof(Get),emp));
+        }
+        //PUT BY ID
+        [HttpPut]
+        public ActionResult<Employee> Put(int id,Employee upemp)
+        {
+           Employee emp=list.SingleOrDefault(x=>x.Id == id);
+            if( emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.FName = upemp.FName;
+                emp.Lname = upemp.Lname;
+                emp.Salary = upemp.Salary;
+                return NoContent ();
+            }
+        }
+    }
+}
---
+++ WebAppApione/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppApione.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public double Salary { get; set; }
+    }
+}
